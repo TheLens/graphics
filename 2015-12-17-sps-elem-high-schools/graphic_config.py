8ecd8f9e8f9a4a5049e522f2f5c158cff386dd28
Thomas Thoren <thomasjthoren@gmail.com>
2015-12-17 16:11:51 -0600

    Adds 2015-12-17-sps-elem-high-schools.

---
+++ 2015-12-17-sps-elem-high-schools/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1cGwVl16zttyvuAvnUKXVAMXd-Ro8x81WR5KXgd6pUyI'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
